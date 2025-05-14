eed177713682605c40969bb59ac75060af68543e
Alex <31933549+crypt096@users.noreply.github.com>
2025-05-14 18:10:38 +0200

    Create EmailInput.vue

---
+++ components/EmailInput.vue
@@ -0,0 +1,15 @@
+<template>
+  <provet-input
+    label="Email"
+    error="Test error message"
+    expand
+    required
+    name="email"
+    type="email"
+    placeholder="user@example.com"
+  ></provet-input>
+</template>
+
+<script setup lang="ts">
+import '@provetcloud/web-components/lib/Input';
+</script>
