ddc9b7fdaf329582e3ce9edb38e62cc79c5be1a9
Alex <31933549+crypt096@users.noreply.github.com>
2025-05-14 12:10:57 +0200

    Create test component.

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,34 @@
-<script setup>
-import { ref } from 'vue'
-import '@provetcloud/web-components/lib/Button'
-
-const count = ref(0)
-</script>
-
 <template>
-  <provet-button variant="primary" @click="count++">
-    Count is: {{ count }}
-  </provet-button>
+  <provet-stack gap="m" direction="vertical" align-items="center" justify-content="center">
+    <provet-input
+        v-model="password"
+        :type="showPassword ? 'text' : 'password'"
+        label="Password"
+    >
+      <provet-button
+          slot="end"
+          square
+          @click="togglePasswordVisibility"
+          aria-label="Toggle password visibility"
+          :aria-pressed="showPassword.toString()"
+      >
+        <provet-icon :name="showPassword ? 'interface-edit-off' : 'interface-edit-on'" size="m" />
+      </provet-button>
+    </provet-input>
+  </provet-stack>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import '@provetcloud/web-components/lib/Stack'
+import '@provetcloud/web-components/lib/Input'
+import '@provetcloud/web-components/lib/Button'
+import '@provetcloud/web-components/lib/Icon'
+
+const password = ref('')
+const showPassword = ref(false)
+
+const togglePasswordVisibility = () => {
+  showPassword.value = !showPassword.value
+}
+</script>
