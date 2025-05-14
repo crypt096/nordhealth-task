454701bf981cd0e4de9ebd75df6664883e87e8da
Alex <31933549+crypt096@users.noreply.github.com>
2025-05-14 11:06:34 +0200

    Add NuxtPage and index.vue

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+import '@provetcloud/web-components/lib/Button'
+
+const count = ref(0)
+</script>
+
+<template>
+  <provet-button variant="primary" @click="count++">
+    Count is: {{ count }}
+  </provet-button>
+</template>
