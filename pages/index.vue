f2672c3778caf913cfc7797d161ce6a0eb86fa39
Alex <31933549+crypt096@users.noreply.github.com>
2025-05-14 17:24:29 +0200

    Move code to appropriate file.

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,3 @@
 <template>
-  <provet-stack gap="m" direction="vertical" align-items="center" justify-content="center">
-    <provet-input v-model="password" :type="showPassword ? 'text' : 'password'" label="Password">
-      <provet-button
-        slot="end"
-        square
-        @click="togglePasswordVisibility"
-        aria-label="Toggle password visibility"
-        :aria-pressed="showPassword.toString()"
-      >
-        <provet-icon :name="showPassword ? 'interface-edit-off' : 'interface-edit-on'" size="m" />
-      </provet-button>
-    </provet-input>
-  </provet-stack>
+  Hello this is index page
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue';
-import '@provetcloud/web-components/lib/Stack';
-import '@provetcloud/web-components/lib/Input';
-import '@provetcloud/web-components/lib/Button';
-import '@provetcloud/web-components/lib/Icon';
-
-const password = ref('');
-const showPassword = ref(false);
-
-const togglePasswordVisibility = () => {
-  showPassword.value = !showPassword.value;
-};
-</script>
