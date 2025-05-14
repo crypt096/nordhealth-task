2cb3913d282fc0799249c82bd07d4b597ddc3015
Alex <31933549+crypt096@users.noreply.github.com>
2025-05-14 18:24:01 +0200

    Update signup.vue

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,9 +1,21 @@
 <template>
-  <provet-stack gap="m" direction="vertical" align-items="center" justify-content="center">
-    <PasswordInput />
+  <provet-stack class="n-container-xxs" style="margin: var(--n-space-xl) auto">
+    <provet-card padding="l">
+      <h1 slot="header" class="n-typescale-l">Create an account</h1>
+      <form action="#">
+        <provet-stack gap="l">
+          <EmailInput />
+          <PasswordInput />
+          <provet-button type="submit" expand variant="primary">Sign up</provet-button>
+        </provet-stack>
+      </form>
+    </provet-card>
   </provet-stack>
+
 </template>
 
 <script setup lang="ts">
 import '@provetcloud/web-components/lib/Stack';
+import '@provetcloud/web-components/lib/Button';
+import '@provetcloud/web-components/lib/Card';
 </script>
