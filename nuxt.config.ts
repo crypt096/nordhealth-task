b8d1cc575f55c77ce19f1916ef3e3cb663383e6a
Alex <31933549+crypt096@users.noreply.github.com>
2025-05-14 10:52:01 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  ssr: false,
+  typescript: {
+    strict: true
+  },
+  app: {
+    head: {
+      title: 'Nordhealth Task',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ]
+    }
+  }
 })
