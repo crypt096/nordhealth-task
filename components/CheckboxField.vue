5c05e1524c0d02fe1febb914bbcaea142f81e247
Alex <31933549+crypt096@users.noreply.github.com>
2025-05-17 20:43:58 +0200

    Update CheckboxField.vue

---
+++ components/CheckboxField.vue
@@ -0,0 +1,18 @@
+<template>
+  <provet-checkbox
+    :checked="modelValue"
+    @change="$emit('update:modelValue', $event.target.checked)"
+    :label="label"
+  />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  modelValue: boolean;
+  label: string;
+}>();
+
+defineEmits<{
+  (e: 'update:modelValue', value: boolean): void;
+}>();
+</script>
