982e99eb151103dfaf7893517980bf88dfe80f6c
Alex <31933549+crypt096@users.noreply.github.com>
2025-05-17 09:35:21 +0200

    Update PasswordInput.vue

--- components/PasswordInput.vue
+++ components/PasswordInput.vue
@@ -17,6 +17,7 @@
       <provet-icon :name="showPassword ? 'interface-edit-off' : 'interface-edit-on'" size="m" label="Toggle password visibility" />
     </provet-button>
   </provet-input>
+  <provet-tooltip :id="`${inputId}-tooltip`">Show / hide password</provet-tooltip>
 </template>
 
 <script setup lang="ts">
@@ -24,9 +25,11 @@ import { ref } from 'vue';
 import '@provetcloud/web-components/lib/Input';
 import '@provetcloud/web-components/lib/Button';
 import '@provetcloud/web-components/lib/Icon';
+import '@provetcloud/web-components/lib/Tooltip';
 
 const password = ref('');
 const showPassword = ref(false);
+const inputId = useId();
 
 const togglePasswordVisibility = () => {
   showPassword.value = !showPassword.value;
