acef27e787f785dc5fe1d9fe74543e77fb9480a6
Alex <31933549+crypt096@users.noreply.github.com>
2025-05-18 17:01:30 +0200

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,10 @@ import { defineVitestConfig } from '@nuxt/test-utils/config'
 
 export default defineVitestConfig({
   test: {
+    globals: true,
+    environment: 'happy-dom',
     include: [
       'tests/unit/*.spec.ts',
     ],
-    environment: 'nuxt',
   },
 })
