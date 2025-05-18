063ef59aa72ab06eb77c66b3e2948a8ee529c7bb
Alex <31933549+crypt096@users.noreply.github.com>
2025-05-18 16:33:46 +0200

    Create dummy test.

---
+++ tests/unit/EmailField.spec.ts
@@ -0,0 +1,8 @@
+import EmailField from '../../components/EmailField.vue';
+import { describe, it, expect } from 'vitest';
+
+describe('Email field component', () => {
+  it('renders label and placeholder', () => {
+    expect(true).toBeTruthy();
+  });
+});
