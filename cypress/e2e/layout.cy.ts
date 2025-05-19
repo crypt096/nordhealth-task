5b8368073931ecc26f31da74831a5782b85b3ffc
Alex <31933549+crypt096@users.noreply.github.com>
2025-05-19 09:01:26 +0200

    Create layout.cy.ts

---
+++ cypress/e2e/layout.cy.ts
@@ -0,0 +1,28 @@
+describe('Layout', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('renders the header with title and user controls', () => {
+    cy.get('provet-header').should('exist');
+    cy.contains('h1', 'NordHealth Task').should('be.visible');
+
+    cy.get('provet-header > provet-stack > provet-button').should('exist');
+
+    cy.get('provet-dropdown').should('not.exist');
+  });
+
+  it('renders the footer with GitHub button and copyright', () => {
+    cy.get('provet-footer').should('exist');
+
+    cy.get('provet-footer > provet-stack > provet-button')
+      .shadow()
+      .find('a')
+      .should('have.attr', 'href', 'https://github.com/crypt096/nordhealth-task')
+      .should('have.attr', 'target', '_blank');
+
+    cy.get('provet-footer')
+      .contains('© 2025 Aleksandar Cvjetan')
+      .should('be.visible');
+  });
+});
